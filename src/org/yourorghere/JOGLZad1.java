f8aa8708d52e515ffc40e5bed83cdb30f9d06033
student <student@240-15.INFORMATYKA.local>
2016-11-03 11:07:44 +0100

    Zadanie 2 - las (kod pożyczono od kolegi)

--- src/org/yourorghere/JOGLZad1.java
+++ src/org/yourorghere/JOGLZad1.java
@@ -7,30 +7,31 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.media.opengl.GL;
-import javax.media.opengl.*;
-import static javax.media.opengl.GL.GL_FRONT;
-import static javax.media.opengl.GL.GL_SHININESS;
-import static javax.media.opengl.GL.GL_SPECULAR;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-
-
-
+/**
+ * SimpleJOGL.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
 public class JOGLZad1 implements GLEventListener {
+
+    //statyczne pola okre?laj?ce rotacj? wokó? osi X i Y
     private static float xrot = 0.0f, yrot = 0.0f;
-    static float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczajšce
-    static float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat³o rozproszone
-    static float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat³o odbite
-    static float lightPos[] = { 0.0f, 150.0f, 150.0f, 1.0f };//pozycja ?wiat³a
-    static int inf = 0;
+    public static float ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};//swiat?o otaczajšce
+    public static float diffuseLight[] = {0.7f, 0.7f, 0.7f, 1.0f};//?wiat?o rozproszone
+    public static float specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; //?wiat?o odbite
+    public static float lightPos[] = {0.0f, 150.0f, 150.0f, 1.0f};//pozycja ?wiat?a
+    public static float lightPos2[] = {-150.0f, -150.0f, 0.0f, 1.0f};//pozycja ?wiat?a
 
     public static void main(String[] args) {
         Frame frame = new Frame("Simple JOGL Application");
         GLCanvas canvas = new GLCanvas();
-
         canvas.addGLEventListener(new JOGLZad1());
         frame.add(canvas);
         frame.setSize(640, 480);
@@ -51,38 +52,53 @@ public class JOGLZad1 implements GLEventListener {
                 }).start();
             }
         });
-        frame.addKeyListener(new KeyListener()
-        {
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode() == KeyEvent.VK_UP)
-                xrot -= 2.0f;
-                if(e.getKeyCode() == KeyEvent.VK_DOWN)
-                xrot +=2.0f;
-                if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-                yrot += 2.0f;
-                if(e.getKeyCode() == KeyEvent.VK_LEFT)
-                yrot -=2.0f;
-                
-                if(e.getKeyChar() == 't')
-                    ambientLight=new float[] {ambientLight[0]-0.1f, ambientLight[0]-0.1f, ambientLight[0]-0.1f, 1};
-                if(e.getKeyChar() == 'y')
-                    ambientLight=new float[] {ambientLight[0]+0.1f, ambientLight[0]+0.1f, ambientLight[0]+0.1f, 1};
-                if(e.getKeyChar() == 'g')
-                    diffuseLight=new float[] {diffuseLight[0]-0.1f, diffuseLight[0]-0.1f, diffuseLight[0]-0.1f, 1};
-                if(e.getKeyChar() == 'h')
-                    diffuseLight=new float[] {diffuseLight[0]+0.1f, diffuseLight[0]+0.1f, diffuseLight[0]+0.1f, 1};
-                if(e.getKeyChar() == 'b')
-                    specular=new float[] {specular[0]-0.1f, specular[0]-0.1f, specular[0]-0.1f, 1};
-                if(e.getKeyChar() == 'n')
-                    specular=new float[] {specular[0]+0.1f, specular[0]+0.1f, specular[0]+0.1f, 1};
-                if(e.getKeyChar() == 'p'){
-                    if(inf==0) inf=1;
-                    else inf=0;
+        //Obs?uga klawiszy strza?ek
+        frame.addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    xrot -= 1.0f;
                 }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    xrot += 1.0f;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    yrot += 1.0f;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    yrot -= 1.0f;
+                }
+                if (e.getKeyChar() == 'q') {
+                    ambientLight = new float[]{ambientLight[0] - 0.1f, ambientLight[0] - 0.1f, ambientLight[0] - 0.1f, 1};
+                }
+                if (e.getKeyChar() == 'w') {
+                    ambientLight = new float[]{ambientLight[0] + 0.1f, ambientLight[0] + 0.1f, ambientLight[0] + 0.1f, 1};
+                }
+                if (e.getKeyChar() == 'a') {
+                    diffuseLight = new float[]{diffuseLight[0] - 0.1f, diffuseLight[0] - 0.1f, diffuseLight[0] - 0.1f, 1};
+                }
+                if (e.getKeyChar() == 's') {
+                    diffuseLight = new float[]{diffuseLight[0] + 0.1f, diffuseLight[0] + 0.1f, diffuseLight[0] + 0.1f, 1};
+                }
+                if (e.getKeyChar() == 'z') {
+                    specular = new float[]{specular[0] - 0.1f, specular[0] - 0.1f, specular[0] - 0.1f, 1};
+                }
+                if (e.getKeyChar() == 'x') {
+                    specular = new float[]{specular[0] + 0.1f, specular[0] + 0.1f, specular[0] + 0.1f, 1};
+                }
+                if (e.getKeyChar() == 'n') {
+                    lightPos[3] = 0;
+                }
+                if (e.getKeyChar() == 'm') {
+                    lightPos[3] = 1;
+                }
+
             }
-            public void keyReleased(KeyEvent e){}
-            public void keyTyped(KeyEvent e){}
+
+            public void keyReleased(KeyEvent e) {
+            }
+
+            public void keyTyped(KeyEvent e) {
+            }
         });
         // Center frame
         frame.setLocationRelativeTo(null);
@@ -100,34 +116,37 @@ public class JOGLZad1 implements GLEventListener {
         // Enable VSync
         gl.setSwapInterval(1);
 
+        //warto?ci sk?adowe o?wietlenia i koordynaty ?ród?a ?wiat?a
+        //(czwarty parametr okre?la odleg?o?? ?ród?a:
+        //0.0f-niesko?czona; 1.0f-okre?lona przez pozosta?e parametry)
+        gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
+        //ustawienie parametrów ?ród?a ?wiat?a nr. 0
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientLight, 0); //swiat?o otaczajšce
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0); //?wiat?o rozproszone
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0); //?wiat?o odbite
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0); //pozycja ?wiat?a
+        gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród?a ?wiat?a nr. 0
+
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, ambientLight, 0); //swiat?o otaczajšce
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, diffuseLight, 0); //?wiat?o rozproszone
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPECULAR, specular, 0); //?wiat?o odbite
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, lightPos2, 0); //pozycja ?wiat?a
+        gl.glEnable(GL.GL_LIGHT1); //uaktywnienie ?ród?a ?wiat?a nr. 0
+
+        gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie ?ledzenia kolorów
+        //kolory b?dš ustalane za pomocš glColor
+        gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
+        //Ustawienie jasno?ci i odblaskowo?ci obiektów
+        float specref[] = {1.0f, 1.0f, 1.0f, 1.0f}; //parametry odblaskowo?ci
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, specref, 0);
+
+        gl.glMateriali(GL.GL_FRONT, GL.GL_SHININESS, 128);
+
+        gl.glEnable(GL.GL_DEPTH_TEST);
         // Setup the drawing area and shading mode
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
-        gl.glEnable(GL.GL_CULL_FACE);
-        
-        //warto?ci sk³adowe o?wietlenia i koordynaty ?ród³a ?wiat³a
-        float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczajšce
-        float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat³o rozproszone
-        float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat³o odbite
-        float lightPos[] = { 0.0f, 150.0f, 150.0f, 1.0f };//pozycja ?wiat³a
-        //(czwarty parametr okre?la odleg³o?æ ?ród³a:
-        //0.0f-nieskoñczona; 1.0f-okre?lona przez pozosta³e parametry)
-        gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
-        //ustawienie parametrów ?ród³a ?wiat³a nr. 0
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat³o otaczajšce
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //?wiat³o rozproszone
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat³o odbite
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat³a
-        gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród³a ?wiat³a nr. 0
-        gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie ?ledzenia kolorów
-        //kolory bêdš ustalane za pomocš glColor
-        gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
-        //Ustawienie jasno?ci i odblaskowo?ci obiektów
-        float specref[] = { 1.0f, 1.0f, 1.0f, 1.0f }; //parametry odblaskowo?ci
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR,specref,0);
-        
-        gl.glMateriali(GL.GL_FRONT,GL.GL_SHININESS,128);
-        
+
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -135,7 +154,7 @@ public class JOGLZad1 implements GLEventListener {
         GLU glu = new GLU();
 
         if (height <= 0) { // avoid a divide by zero error!
-        
+
             height = 1;
         }
         final float h = (float) width / (float) height;
@@ -146,57 +165,58 @@ public class JOGLZad1 implements GLEventListener {
         gl.glMatrixMode(GL.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
-    
-    public float[] WyznaczNormalna(float[] punkty, int ind1, int ind2, int ind3) {
-        float[] norm = new float[3];
-        float[] wektor0 = new float[3];
-        float[] wektor1 = new float[3];
-
-        for (int i = 0; i < 3; i++) {
-            wektor0[i] = punkty[i + ind1] - punkty[i + ind2];
-            wektor1[i] = punkty[i + ind2] - punkty[i + ind3];
-        }
-
-        norm[0] = wektor0[1] * wektor1[2] - wektor0[2] * wektor1[1];
-        norm[1] = wektor0[2] * wektor1[0] - wektor0[0] * wektor1[2];
-        norm[2] = wektor0[0] * wektor1[1] - wektor0[1] * wektor1[0];
-        float d
-                = (float) Math.sqrt((norm[0] * norm[0]) + (norm[1] * norm[1]) + (norm[2] * norm[2]));
-        if (d == 0.0f) {
-            d = 1.0f;
-        }
-        norm[0] /= d;
-        norm[1] /= d;
-        norm[2] /= d;
-
-        return norm;
-    }
 
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
 
-        // Clear the drawing area
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-       
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.0f, 0.0f, -6.0f); //przesuniêcie o 6 jednostek
-        gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó³ osi X
-        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó³ osi Y
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,inf); //swiat³o otaczajšce
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,inf); //?wiat³o rozproszone
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,inf); //?wiat³o odbite
-        gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,inf); //pozycja ?wiat³a
-       /* 
-        gl.glLightfv(GL.GL_LIGHT1,GL.GL_AMBIENT,ambientLight,inf); //swiat³o otaczajšce
-        gl.glLightfv(GL.GL_LIGHT1,GL.GL_DIFFUSE,diffuseLight,inf); //?wiat³o rozproszone
-        gl.glLightfv(GL.GL_LIGHT1,GL.GL_SPECULAR,specular,inf); //?wiat³o odbite
-        gl.glLightfv(GL.GL_LIGHT1,GL.GL_POSITION,lightPos,inf); //pozycja ?wiat³a
-        */
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientLight, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0);
 
-        rysujDrzewo(gl);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, 0.0f, -6.0f); //przesuni?cie o 6 jednostek
+        gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó? osi X
+        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó? osi Y
+
         gl.glFlush();
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        gl.glScalef(0.3f, 0.3f, 0.3f);
+        gl.glTranslatef(-7.5f, -7.5f, 0.0f);
+        gl.glPushMatrix();
+        
+        for (int j = 0; j < 10; j++) {
+            gl.glPushMatrix();
+            for (int i = 0; i < 10; i++) {
+                drzewo(gl);
+                gl.glTranslatef(0.0f, 1.5f, 0.0f);
+            }
+            gl.glPopMatrix();
+            gl.glTranslatef(1.5f, 0.0f, 0.0f);
+        }
+        gl.glPopMatrix();
+        gl.glTranslatef(-7.5f, -7.5f, 0.0f);
+
     }
-    
+    void drzewo(GL gl) {
+        gl.glColor3f(0.14f,0.55f,0.13f);
+        gl.glPushMatrix();
+        gl.glTranslatef(0.0f, 0.0f, -1.0f);
+        gl.glScalef(0.4f, 0.4f, 0.4f);
+        stozek(gl);
+        gl.glTranslatef(0.0f, 0.0f, +1.8f);
+        gl.glScalef(1.4f, 1.4f, 1.4f);
+        stozek(gl);
+        gl.glTranslatef(0.0f, 0.0f, +1.3f);
+        gl.glScalef(1.2f, 1.2f, 1.2f);
+        stozek(gl);
+        gl.glTranslatef(0.0f, 0.0f, 1.0f);
+        gl.glColor3f(0.55f,0.27f,0.08f);
+        gl.glScalef(0.6f, 0.6f, 1.0f);
+        walec(gl);
+        gl.glPopMatrix();
+    }
     public void rysowanieMorgenszternu(GL gl){
         float kat, p, q;
         float x=0.0f;
@@ -517,7 +537,7 @@ public class JOGLZad1 implements GLEventListener {
 
     
     
-    public void rysowanieWalca(GL gl){
+  /*  public void rysowanieWalca(GL gl){
         float kat, p, q;
         float x=0.0f;
         float z=0.0f;
@@ -736,6 +756,10 @@ public class JOGLZad1 implements GLEventListener {
     }
     
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }*/
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
 
